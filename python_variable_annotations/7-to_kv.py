a69693300049aa59d3b8cb4c9019c574b1bebbb6
Adam <adamdiallo2k@gmail.com>
2023-08-28 06:26:52 -0700

    task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" commented module """
+from typing import List, Union, Tuple
+
+
+def sum_mixed_list(input_list: List[Union[int, float]]) ->  tuple(str, float):
+    """ Takes a list of floats as argument
+    and returns the sum of the floats. """
+    return tuple(input_list)
