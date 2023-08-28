b4043799b2c240418d83d4b5b25dd0563fb87f23
Adam <adamdiallo2k@gmail.com>
2023-08-28 06:36:08 -0700

    task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" commented module """
+from typing import Union, Tuple
+
+
+def make_multiplier(multiplier: float) -> float
+    """ commented function """
+    return (multiplier ** 2)
