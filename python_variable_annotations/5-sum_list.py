3287a669801a9d835d38075cc2d462de640a19ae
Adam <adamdiallo2k@gmail.com>
2023-08-28 05:01:29 -0700

    task 5

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """ commented module """
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """ Takes a list of floats as argument
     and returns the sum of the floats. """
