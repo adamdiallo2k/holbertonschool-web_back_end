d6b9a558cff743c0477f88bad0efc295a14eddb8
Adam <adamdiallo2k@gmail.com>
2023-08-28 05:08:29 -0700

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" commented module """
+from typing import List, Union
+
+
+def sum_mixed_list(input_list: List[Union[int, float]]) -> float:
+    """ Takes a list of floats as argument
+    and returns the sum of the floats. """
+    return (input_list)
