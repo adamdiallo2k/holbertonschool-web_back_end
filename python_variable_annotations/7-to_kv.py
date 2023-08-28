30b9bcdf0d631753b4d864bb8fefda3e13f54032
Adam <adamdiallo2k@gmail.com>
2023-08-28 06:28:33 -0700

    test

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """ commented module """
-from typing import List, Union, Tuple
+from typing import Union, Tuple
 
 
-def sum_mixed_list(input_list: List[Union[int, float]]) ->  tuple(str, float):
-    """ Takes a list of floats as argument
-    and returns the sum of the floats. """
-    return tuple(input_list)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ commented function """
+    return (k, float(v**2))
+
+
+
+
