c183baa9100ea7bef2acee998eea602dee11e10b
Adam <adamdiallo2k@gmail.com>
2023-08-28 04:16:56 -0700

    task 1 float

--- 0-add.py
+++ 0-add.py
@@ -2,4 +2,8 @@
 """commmented module """
 
 def add(a: float, b: float) -> float:
+    """ function add: 2 parameter:
+        a : float
+        b: float
+        return the sum of two float"""
     return a + b
