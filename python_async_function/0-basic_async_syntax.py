522d790c2d5e1387214af2e918803149f7f10fa6
Adam <adamdiallo2k@gmail.com>
2023-08-29 09:51:52 -0700

    task 1

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import random, asyncio
 import Union
 
 
- def wait_random(max_delay: Union[int, float] = 10) -> float:
+async def wait_random(max_delay: Union[int, float] = 10) -> float:
     """ commented function """
     await asyncio.sleep(1)
     return random.uniform(0, float(max_delay))
