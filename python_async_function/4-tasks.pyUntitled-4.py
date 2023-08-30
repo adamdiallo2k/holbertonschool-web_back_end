06a6ea00678d267b71b9bc6b9e74239443dc7a1b
Adam <adamdiallo2k@gmail.com>
2023-08-30 14:00:16 -0700

    task 4

---
+++ python_async_function/4-tasks.pyUntitled-4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""measures the total execution time for wait_n"""
+
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Return an asyncio Task"""
+    return asyncio.create_task(wait_random(max_delay))
+
+async def task_wait_n(n: int, max_delay: int):
+    """Use task_wait_random to create multiple asyncio Tasks"""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
