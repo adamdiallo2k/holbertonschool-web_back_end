bdcbf2583a98243e9dcc819fe50f89d28e3a6fd4
Adam <adamdiallo2k@gmail.com>
2023-08-30 13:36:19 -0700

    test

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -7,10 +7,12 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def measure_time(n: int, max_delay: int) -> float:
-    """ commented function """
+    """Measure total execution time for wait_n(n, max_delay)"""
     start_time = time.time()
-    await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+
+
+    await wait_n(n, max_delay)
+
     end_time = time.time()
-    Total_time = end_time - start_time
-    Total_time = Total_time / n
-    return total_time
+    total_time = end_time - start_time
+    return total_time / n
