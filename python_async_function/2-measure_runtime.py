398304ce223451fec31879eb4d6129f299716095
Adam <adamdiallo2k@gmail.com>
2023-08-30 13:42:35 -0700

    redo the task

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-""" commented module """
-
-
-import asyncio
+"""measures the total execution time for wait_n"""
 import time
+import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the average execution time for the `wait_n` function.
 
