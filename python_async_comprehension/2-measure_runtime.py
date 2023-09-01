b531967dce8f5aded443d93e184aab016b8adc90
Adam <adamdiallo2k@gmail.com>
2023-09-01 12:21:37 -0700

    task 3

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" commented module """
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    """Measure the total runtime of executing async_comprehension four times in parallel."""
+    start_time = asyncio.get_event_loop().time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = asyncio.get_event_loop().time()
+    return end_time - start_time
