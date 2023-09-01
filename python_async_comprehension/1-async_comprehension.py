bd50e6f53cc54ed07dc75198f96d4ab786e899ea
Adam <adamdiallo2k@gmail.com>
2023-09-01 12:11:50 -0700

    test

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,8 +4,9 @@
 
 import asyncio
 import random
+From Typing import List
 async_generator = __import__('0-async_generator').async_generator
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """ commented function """
     return [number async for number in async_generator()]
