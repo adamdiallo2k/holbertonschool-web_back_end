3ba835e92869a985342497cf32993cbb65a17b7b
Adam <adamdiallo2k@gmail.com>
2023-09-05 02:50:43 -0700

    add a ducument variable and add list function to display the result

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ commented function """
 
-
 def list_all(mongo_collection):
     """ that lists all documents in a collection: """
-    if not db.school.find():
+    documents = mongo_collection.find()
+    if documents.count() == 0:
         return []
     else:
-        return db.school.find()
+        return list(documents)
