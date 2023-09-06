6f82a562a96eac78384ac4269795718ab27f3c08
Adam <adamdiallo2k@gmail.com>
2023-09-06 02:57:07 -0700

    test

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -13,10 +13,5 @@ def schools_by_topic(mongo_collection, topic):
     Returns:
     - List of schools with the specified topic.
     """
-    
     schools = mongo_collection.find({"topics": topic})
-    
-    # Convert the cursor to a list of school names
-    school_list = [school['name'] for school in schools if 'name' in school]
-    
-    return school_list
+    return schools
