abfdeb7f1148e71e07af31952120fbd28aa14900
Adam <adamdiallo2k@gmail.com>
2023-09-15 14:04:52 -0700

    task 11, 12

---
+++ ES6_basic/12-createReportObject.js.ts
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  employeesList = {
+    allEmployees: {
+      engineering: [
+                'John Doe',
+                'Guillaume Salva',
+           ],
+        },
+      };
+}
