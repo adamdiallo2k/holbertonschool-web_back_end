aa80fa824ba67ac7465fed342324189b2f52fb81
Adam <adamdiallo2k@gmail.com>
2023-10-08 18:56:31 -0700

    task 1

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,14 @@
 function getListStudents() {
-    // Create an array of student objects
-    const students = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' }
+
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
     ];
     
-    // Return the array of student objects
-    return students;
-  }
+// Return the array of student objects
+  return students;
+}
   
-  // Export the function as a default export
-  export default getListStudents;
-  
+// Export the function as a default export
+export default getListStudents;
