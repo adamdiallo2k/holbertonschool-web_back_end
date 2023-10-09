a75a577fbf31552864f6fed3bb20c2f5983590b1
Adam <adamdiallo2k@gmail.com>
2023-10-08 19:02:55 -0700

    task 1

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -4,11 +4,11 @@ function getListStudents() {
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
-    
-// Return the array of student objects
+  ];
+
+  // Return the array of student objects
   return students;
 }
-  
+
 // Export the function as a default export
 export default getListStudents;
